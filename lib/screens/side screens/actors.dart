271221d3b2831d60aac40016b3ff9f655ba5ab6c
Yasar Mushtaq <ymushtaq.01@gmail.com>
2024-05-10 17:48:50 +0600

    actor and stuff added

--- lib/screens/side screens/actors.dart
+++ lib/screens/side screens/actors.dart
@@ -1,109 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:vinlandsaga_pro/widgets/actor/actor_feed.dart';
+import 'package:vinlandsaga_pro/widgets/actor/new_actor.dart'; 
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ActorsPage extends StatelessWidget {
-  const ActorsPage({super.key});
+class Actorcreen extends StatefulWidget {
+  // ignore: use_super_parameters
+  const Actorcreen({Key? key}) : super(key: key);
 
-  
+  @override
+  State<Actorcreen> createState() => _ActorcreenState();
+}
+
+class _ActorcreenState extends State<Actorcreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('Stuff');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Actors'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: PageView(
-                  children: const <Widget>[
-                    CastCard(
-                      image: 'assets/images/actors/Thorfinn.jpg',
-                      characterName: 'Thorfinn',
-                    ),
-                    CastCard(
-                      image: 'assets/images/actors/ketil.jpg',
-                      characterName: 'Ketil',
-                    ),
-                    CastCard(
-                      image: 'assets/images/actors/Canut.png',
-                      characterName: 'Canut',
-                    ),
-                    CastCard(
-                      image: 'assets/images/actors/Thorkell.png',
-                      characterName: 'Thorkell',
-                    ),
-                    CastCard(
-                      image: 'assets/images/actors/Einar.png',
-                      characterName: 'Einar',
-                    ),
-                    CastCard(
-                      image: 'assets/images/actors/Askeladd.png',
-                      characterName: 'Askeladd',
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 10), // Add some spacing from top
+            NewActor(), // Updated widget reference
+            Expanded(
+              child: ActorFeed(),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-class CastCard extends StatelessWidget {
-  final String image;
-  final String characterName;
-
-  const CastCard({super.key, 
-    required this.image,
-    required this.characterName,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 4.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: SizedBox(
-          width: 400.0,
-          height: 400.0,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(image),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                width: double.infinity,
-                color: Colors.white,
-                child: Text(
-                  characterName,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-//final 
--- lib/screens/side screens/stuff.dart
+++ lib/screens/side screens/stuff.dart
@@ -1,128 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:vinlandsaga_pro/widgets/stuff/new_stuff.dart';
+import 'package:vinlandsaga_pro/widgets/stuff/stuff_feed.dart'; 
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class StuffPage extends StatelessWidget {
-  const StuffPage({Key? key}) : super(key: key);
+class StuffScreen extends StatefulWidget {
+  // ignore: use_super_parameters
+  const StuffScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StuffScreen> createState() => _StuffScreenState();
+}
+
+class _StuffScreenState extends State<StuffScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('Stuff');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stuff'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: PageView(
-                  children: const <Widget>[
-                    CastCard(
-                      realName: 'Aleks le',
-                      characterName: 'Thorfinn',
-                      image: 'assets/images/stuff/AleksLe-Thorfinn.jpg',
-                    ),
-                    CastCard(
-                      realName: 'Hideaki Teuka',
-                      characterName: 'Ketil',
-                      image: 'assets/images/stuff/HideakiTeuka-ketil.jpg',
-                    ),
-                    CastCard(
-                      realName: 'Kensho Ono',
-                      characterName: 'Canute',
-                      image: 'assets/images/stuff/KenshoOno-Canute.jpg',
-                    ),
-                    CastCard(
-                      realName: 'Akio Ohtsuka',
-                      characterName: 'Thorkell',
-                      image: 'assets/images/stuff/AkioOhtsuka-Thorkell.jpg',
-                    ),
-                    CastCard(
-                      realName: 'AlejandroSaab',
-                      characterName: 'Einar',
-                      image: 'assets/images/stuff/AlejandroSaab-Einar.jpg',
-                    ),
-                    CastCard(
-                      realName: 'Naoya Uchida',
-                      characterName: 'Askeladd',
-                      image: 'assets/images/stuff/NaoyaUchida-Askeladd.jpg',
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: 10), // Add some spacing from top
+            NewStuff(), // Updated widget reference
+            Expanded(
+              child: StuffFeed(),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-class CastCard extends StatelessWidget {
-  final String realName;
-  final String characterName;
-  final String image;
-
-  const CastCard({
-    Key? key,
-    required this.realName,
-    required this.characterName,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        elevation: 4.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: SizedBox(
-          width: 400.0,
-          height: 400.0,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(image),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      characterName,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 4.0),
-                    Text(
-                      'Real Name: $realName',
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-//final 
