6307b7ebc11955b44868c6a71f5ea21bfceb451e
Yasar Mushtaq <ymushtaq.01@gmail.com>
2024-05-10 15:05:12 +0600

    FireBase connection added

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,42 @@
+import 'package:vinlandsaga_pro/screens/auth.dart';
+import 'package:vinlandsaga_pro/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'screens/welcome/welcome.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'widgets/botton_navigation.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Vinland Saga',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 211, 113, 0)),
-        useMaterial3: true,
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 211, 113, 0),),
       ),
-      home: const WelcomePage(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const Bottom();
+          }
+          return const AuthScreen();
+        },
+      ),
     );
   }
 }
-//final 
